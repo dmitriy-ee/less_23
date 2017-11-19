20e5a4e194a5b629d9e93c85783a6890f27b4a93
dmitriy_ee <dmitriy_ee@hotmail.com>
2017-11-19 13:21:34 +0200

    post info recieve

--- app_0.rb
+++ app_0.rb
@@ -5,3 +5,16 @@ require 'sinatra/reloader'
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@bb_bitch = params[:bb_bitch]
+
+	erb "Information recive!\n 
+	Username: #{@username}\n 
+	Phone: #{@phone}\n 
+	Datetime: #{@datetime}\n 
+	Barberbitch: #{@bb_bitch}\n"
+end
